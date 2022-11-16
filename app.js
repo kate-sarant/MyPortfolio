5dfc28c3a08424c697c48a05983f185d63640cdb
kate-sarant <kate_sarant@yahoo.gr>
2022-11-16 19:06:10 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// 
+// var cv = document.querySelector(".CVBtn");
+// function printCV(){
+//    window.location.href ='assets/CV-Katerina_Sarantopoulou.pdf'.open();
+//
+//
+// }
+// cv.onClick('printCV');
